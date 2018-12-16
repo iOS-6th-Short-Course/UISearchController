2925b60a6677f2ee9a84f800223f3debfa7c956b
Chhaileng Peng <chhailengpeng@gmail.com>
2018-12-16 09:51:11 +0700

    Initial Commit

---
+++ UISearchController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISearchController
+//
+//  Created by Chhaileng Peng on 12/16/18.
+//  Copyright © 2018 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
